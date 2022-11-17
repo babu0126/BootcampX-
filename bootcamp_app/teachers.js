e35a6fa48db54a7ae79e661b4002fa15a29a8b5b
David Wang <18davidwang@gmail.com>
2022-11-17 01:04:25 -0800

    Formating document

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -23,7 +23,4 @@ ORDER BY teacher;
   res.rows.forEach((user) => {
     console.log(`${user.cohort}: ${user.teacher}`);
   });
-  // res.rows.forEach(user => {
-  //   console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  // })
 }).catch(err => console.error('query error', err.stack));
